553e15200fa9eb45b7a17b352c0ee14cdf910ef1
apple0c <73928043+apple0c@users.noreply.github.com>
2021-01-16 22:43:02 +0800

    Update xp.js

--- xp.js
+++ xp.js
@@ -187,7 +187,9 @@ return new Promise((resolve, reject) => {
    $.get(balanceurl,(error, response, data) =>{
      const result = JSON.parse(data)
         if(logs)$.log(data)
-     message += '金币余额：'+result.data.coinSum+'\n现金余额：'+result.data.balanceSum/100+'\n'
+     //message += '金币余额：'+result.data.coinSum+'\n现金余额：'+result.data.balanceSum/100+'\n'
+     message += '金币余额：'+'\n现金余额：'+result.data.balanceSum/100+'\n'
+
     coins = result.data.balanceSum/100;
           resolve()
     })
